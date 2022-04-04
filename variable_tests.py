4816fe25c09ae829c2b1dd2305ba3551b01dacfd
Tomislav Mamić <tomislav.mamic1@gmail.com>
2022-04-04 19:26:31 +0200

    Also check that comparison between different FBDVars fails.

--- variable_tests.py
+++ variable_tests.py
@@ -17,12 +17,18 @@ class VariableTests(unittest.TestCase):
     def test_dump_and_load(self):
         var = FBDVar("tstvar", "dtype")
         img = FBDVar.load(var.dump())
+
         # confirm that base data content is intact
         for item in FBDVar.DATAMODEL:
             self.assertEqual(var.__dict__[item], img.__dict__[item])
+
         # ensure that var and image are equal, but not the same object
         self.assertEqual(var, img)
         self.assertIsNot(var, img)
+
+        # ensure that different variables are not seen as equal
+        self.assertNotEqual(var, FBDVar("not tstvar", "dtype"))
+        self.assertNotEqual(var, FBDVar("tstvar", "not dtype"))
 
 if __name__ == "__main__":
     unittest.main()
