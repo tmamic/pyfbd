4e38c8facd46b629e7f218bd81774599eed706af
Tomislav Mamić <tomislav.mamic1@gmail.com>
2022-04-05 19:01:55 +0200

    Mean tests to catch false equivalence between diagrams.

--- diagram_tests.py
+++ diagram_tests.py
@@ -11,6 +11,7 @@ import unittest
 # internal
 from pyfbd.diagram import FBDiagram
 from pyfbd.function import FBDFunc
+from pyfbd.variable import FBDVar
 
 class DiagramTests(unittest.TestCase):
     """All unit tests for function.py should be contained here."""
@@ -18,16 +19,28 @@ class DiagramTests(unittest.TestCase):
     def test_dump_and_load(self):
         diagram = FBDiagram()
         func = FBDFunc("tstfunc", tuple(), tuple(), tuple())
-        uid1 = diagram.add_function(func)
+        _ = diagram.add_function(func)
         img = FBDiagram.load(diagram.store())
-        uid2 = img.add_function(func)
-
-        # ensure that unique IDs persist across multiple instances of the same module
-        self.assertNotEqual(uid1, uid2)
 
         # ensure object equivalence, but not identity
         self.assertIsNot(diagram, img)
         self.assertEqual(diagram, img)
+
+    def test_topological_equivalence(self):
+        # make sure that diagrams with different functions are not equivalent
+        sch1 = FBDiagram()
+        sch2 = FBDiagram()
+        var1 = FBDVar("tstvar", "dtype")
+        var2 = FBDVar("not tstvar", "dtype")
+        f1 = FBDFunc("tstf1", (var1,), tuple(), tuple())
+        f2 = FBDFunc("tstf1", (var2,), tuple(), tuple())
+
+        _ = sch1.add_function(f1)
+        _ = sch2.add_function(f1)
+        self.assertEqual(sch1, sch2)
+
+        _ = sch1.add_function(f2)
+        self.assertNotEqual(sch1, sch2)
 
     def test_uid(self):
         diagram = FBDiagram()
