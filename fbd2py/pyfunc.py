3084896616f87cf77a735bf2ce63cd85dcc7d330
Tomislav Mamić <tomislav.mamic1@gmail.com>
2022-04-06 18:23:09 +0200

    [3] Started the python extension of FBDFunc.

---
+++ fbd2py/pyfunc.py
@@ -0,0 +1,12 @@
+"""
+@what:  fbd2py extension of generic function block class.
+@why:   This class tells the compiler how to write python functions.
+@who:   TM
+@when:  2022-04-06
+"""
+
+# internal
+from pyfbd.function import FBDFunc
+
+class PyFunc(FBDFunc):
+    pass
