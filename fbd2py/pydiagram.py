31344ffb8df37106855aaf7dcff806aefd4ef705
Tomislav Mamić <tomislav.mamic1@gmail.com>
2022-04-06 18:25:01 +0200

    [3] Started python extension of FBDiagram.

---
+++ fbd2py/pydiagram.py
@@ -0,0 +1,12 @@
+"""
+@what:  fbd2py extension of generic FBD class.
+@why:   This class tells the compiler how to convert diagrams to python code.
+@who:   TM
+@when:  2022-04-06
+"""
+
+# internal
+from pyfbd.diagram import FBDiagram
+
+class PyDiagram(FBDiagram):
+    pass
