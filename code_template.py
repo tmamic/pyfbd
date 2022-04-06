5ea628333b9bf65439fc64752901bb60f6eb42cb
Tomislav Mamić <tomislav.mamic1@gmail.com>
2022-04-06 19:27:28 +0200

    [3] Template-filling utilities.

---
+++ code_template.py
@@ -0,0 +1,26 @@
+"""
+@what:  Code template utilities for use in compiler extensions.
+@why:   Maintain uniform template format, keep things extensible.
+@who:   TM
+@when:  2022-04-06
+"""
+
+# built-in
+import json
+import re
+
+def load_template(fname: str) -> dict:
+    """Load a json template. For now, don't do anything smart with it."""
+    with open(fname, "r", encoding="utf-8") as template:
+        return json.load(template)
+
+TAG_REGEX = re.compile(r"<!(.*)>")
+def _process_tag(match) -> str:
+    """Utility function - convert regex match to tag string."""
+    # group(1) is the first subgroup in TAG_REGEX, 0 is the entire match
+    return match.group(1)
+
+def fill_section(section: "list[str]", data: dict) -> str:
+    """Parse the section lines for keys and fill them in with data."""
+    ret = "\n".join(section)
+    return re.sub(TAG_REGEX, lambda tag: data[_process_tag(tag)], ret)
