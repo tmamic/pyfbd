068de430807352654b36ae96aa97d6d95a2787ed
Tomislav Mamić <tomislav.mamic1@gmail.com>
2022-04-06 18:21:07 +0200

    [3] Started the python extension of FBDVar.

---
+++ fbd2py/pyvar.py
@@ -0,0 +1,12 @@
+"""
+@what:  fbd2py extension of generic variable class.
+@why:   This class tells the compiler how to write python variables.
+@who:   TM
+@when:  2022-04-06
+"""
+
+# internal
+from pyfbd.variable import FBDVar
+
+class PyVar(FBDVar):
+    pass
