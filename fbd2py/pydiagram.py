80030f18ebe7c937c2149e75a61c5e205e532c47
Tomislav Mamić <tomislav.mamic1@gmail.com>
2022-04-09 14:09:13 +0200

    [3] Scrapped the initial code generation scheme.

--- fbd2py/pydiagram.py
+++ fbd2py/pydiagram.py
@@ -15,33 +15,11 @@ from pyfbd import code_template
 
 class PyDiagram(FBDiagram):
 
-    def make_header(self, template: dict) -> str:
-        """Fill in the file header."""
-        data = {'source': "spoof",
-                'dt': datetime.now(),
-                'unique_fbs': len(self._unique_functions),
-                'nfbs': len(self.function_blocks),
-                'nconn': len(self.connections)}
-        lines = code_template.fill_section(template['header'], data)
-        return "\n".join(lines)
-
-    def make_class(self, template: dict) -> str:
-        """Fill in the class content."""
-        data = {'source': "spoof"}
-        lines = code_template.fill_section(template['class_header'], data)
-        for item in self._unique_functions:
-            func = PyFunc.load(item.dump())
-            for line in func.compile():
-                lines.append("    " + line)
-        return "\n".join(lines)
-
     def compile(self, fname: str) -> None:
         """Converts the data content of this diagram into python code."""
         if not fname.endswith(".py"):
             fname += ".py"
 
-        template = code_template.load_template("fbd2py/diagram_template.json")
+        template = code_template.load_template("fbd2py/diagram.fbdt")
         with open(fname, "w", encoding="utf-8") as outfile:
-            outfile.write(self.make_header(template))
-            outfile.write("\n\n")
-            outfile.write(self.make_class(template))
+            return
--- fbd2py/pyfunc.py
+++ fbd2py/pyfunc.py
@@ -11,20 +11,6 @@ from pyfbd import code_template
 
 class PyFunc(FBDFunc):
 
-    def add_output_def(self, out: list) -> None:
-        """Appends the definition of function outputs to given out list."""
-        outputs = list(self.outputs.keys())
-        if not outputs:
-            out.append(f"    ret = None")
-            return
-
-        cur = outputs.pop(0)
-        out.append(f"    ret = {{'{cur}': None")
-        while outputs:
-            cur = outputs.pop(0)
-            out.append(f"           '{cur}': None")
-        out[-1] += "}"
-
     def get_template(self) -> dict:
         """Load function implementation template from file."""
         template_name = f"fbd2py/{self.name}_template.json"
@@ -32,22 +18,3 @@ class PyFunc(FBDFunc):
 
     def compile(self) -> str:
         """Converts the data content of this function into python code."""
-        args = ["cls"]
-        if self.inputs:
-            args.append("inputs")
-        if self.state:
-            args.append("state")
-        lines = [f"def {self.name}({', '.join(args)}):"]
-
-        template = self.get_template()
-        for line in code_template.fill_section(template['docstring'], {}):
-            lines.append(f"    {line}")
-
-        self.add_output_def(lines)
-
-        for line in code_template.fill_section(template['body'], {}):
-            lines.append(f"    {line}")
-
-        lines.append("    return ret")
-
-        return lines
