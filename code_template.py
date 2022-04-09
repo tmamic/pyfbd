113e9f0714b690b7c074b5a1f900f9f33d18d919
Tomislav Mamić <tomislav.mamic1@gmail.com>
2022-04-09 14:09:36 +0200

    Started work on a more elegant section-based scheme.

--- code_template.py
+++ code_template.py
@@ -6,13 +6,55 @@
 """
 
 # built-in
-import json
 import re
+from dataclasses import dataclass
+
+@dataclass
+class Section:
+    """Keep track of section attributes."""
+    name: str
+    type: str
+    properties: dict
+    content: str
+
+SECTION_REGEX = re.compile(r"<#(.*?):(.*)>")
+
+def get_args(argstr: str) -> dict:
+    """Parse a string of comma-separated arguments into key-value pairs."""
+    ret = {}
+    for pair in argstr.split(","):
+        try:
+            key, val = pair.split("=")
+        except ValueError:
+            key, val = pair, ""
+        ret[key.strip()] = val.strip()
+    return ret
+
+def parse_section(sect: tuple) -> Section:
+    """Parse section attributes."""
+    stype, argstr = sect
+    args = argstr.split(",")
+    sname = args.pop(0).strip()
+    props = {}
+    while args:
+        pair = args.pop(0)
+        key, val = pair.split("=")
+        props[key.strip()] = val.strip()
+    return Section(sname, stype, props, "")
+
+def iter_sections(text: str):
+    """Iteratively returns section objects from a file segment."""
+    for sect in re.findall(SECTION_REGEX, text):
+        yield parse_section(sect)
 
 def load_template(fname: str) -> dict:
-    """Load a json template. For now, don't do anything smart with it."""
+    """Load a code template."""
+    ret = {'fname': fname, 'sections': []}
     with open(fname, "r", encoding="utf-8") as template:
-        return json.load(template)
+        for sect in iter_sections(template.read()):
+            ret['sections'].append(sect)
+    print(ret)
+    return ret
 
 TAG_REGEX = re.compile(r"<!(.*)>")
 def _process_tag(match) -> str:
@@ -21,5 +63,5 @@ def _process_tag(match) -> str:
     return match.group(1)
 
 def fill_section(section: "list[str]", data: dict) -> str:
-    """Parse the section lines for keys and fill them in with data."""
+    """Parse the section for keys and fill them in with data."""
     return [re.sub(TAG_REGEX, lambda tag: str(data[_process_tag(tag)]), line) for line in section]
