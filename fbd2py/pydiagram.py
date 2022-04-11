e5b71e693826db4db93e17faa6c56f097a24c2f6
Tomislav Mamić <tomislav.mamic1@gmail.com>
2022-04-11 21:51:56 +0200

    Half-assed section filling algorithm.

--- fbd2py/pydiagram.py
+++ fbd2py/pydiagram.py
@@ -33,6 +33,28 @@ class PyDiagram(FBDiagram):
             code_template.fill_section(sect, global_data)
             print(f"{sect.name}:\n---\n{sect.content}")
 
+        input_sections = {sect.name: "" for sect in template.iter_sections_by_type("in")}
+        for func in self._unique_functions:
+            pyfunc_obj = PyFunc.load(func.dump())
+            function_outs = pyfunc_obj.compile_sections()
+            print(f"[INFO] Compiling outputs for {func.name}.")
+            for sect_name, content in function_outs.items():
+                section = template.get_section_by_name(sect_name)
+                indent = 0
+                if 'indent' in section.properties:
+                    indent = int(section.properties['indent'])
+                if sect_name in input_sections:
+                    for line in content.splitlines():
+                        indented = (" " * indent + line) + "\n"
+                        section.content += indented
+                        input_sections[sect_name] += indented
+
+        for sect, content in input_sections.items():
+            if not content:
+                print(f"[INFO] Section {sect} empty.")
+        for sect in template.sections:
+            code_template.fill_section(sect, input_sections)
+
         with open(fname, "w", encoding="utf-8") as outfile:
             for sect in template.sections:
                 filler = "." * (50 - len(sect.name))
